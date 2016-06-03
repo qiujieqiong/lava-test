fe36de02c70d695c6c5226683e4e93fab483efac
qiujieqiong@linuxdeepin.com <qiujieqiong@linuxdeepin.com>
2016-06-03 12:30:32 +0800

    Change home dir to Generate Xauthority

--- shelltest.sh
+++ shelltest.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ls -ahl /home
-sudo xauth generate :0 . trusted
-ls -ahl /home
+cd /home/AUTO_LOGIN_USER && sudo xauth generate :0 . trusted
+ls -ahl /home/AUTO_LOGIN_USER
 env
 export DISPLAY=:0
 env
