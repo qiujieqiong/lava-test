8d9c66b40e4f9b50688f17e27f957cfb18bf1764
qiujieqiong <qiujieqiong@deepin.com>
2016-07-01 10:58:59 +0800

    Add echo

--- nopasswd.sh
+++ nopasswd.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-
+set -x
+cat /etc/lightdm/lightdm.conf.d/auto-login.conf
+echo $AUTO_LOGIN_USER
 echo "deepin  ALL=(ALL) NOPASSWD:ALL" >>/etc/sudoers
-
+id deepin
+groups deepin
 cat /etc/sudoers
